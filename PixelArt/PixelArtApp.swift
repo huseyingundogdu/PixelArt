dfa9193051e057946efdda0c80733f621cae312f
Huseyin Gundogdu <73406964+huseyingundogdu@users.noreply.github.com>
2025-02-04 02:35:09 +0100

    Initial Commit

---
+++ PixelArt/PixelArtApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelArtApp.swift
+//  PixelArt
+//
+//  Created by Hüseyin Gündoğdu on 04/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct PixelArtApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
