17285cd38f0daeb55fee4015720466355eef9e2e
Huseyin Gundogdu <73406964+huseyingundogdu@users.noreply.github.com>
2025-07-17 03:29:14 +0300

    feat: add see artwork full screen feature

--- PixelArt/Shared/UI/ArtworkViewer.swift
+++ PixelArt/Shared/UI/ArtworkViewer.swift
@@ -8,7 +8,7 @@
 import SwiftUI
 
 struct ArtworkViewer: View {
-    
+    @State private var showFullScreen: Bool = false
     let artwork: Artwork
     
     var body: some View {
@@ -28,7 +28,20 @@ struct ArtworkViewer: View {
         .padding(4)
         .border(.black, width: 4)
         .pixelBackground(paddingValue: 10)
-
+        .overlay(alignment: .topTrailing) {
+            Button {
+                showFullScreen = true
+            } label: {
+                Image("ic_eye")
+                    .resizable()
+                    .interpolation(.none)
+                    .frame(width: 30, height: 30)
+                    .padding()
+            }
+        }
+        .fullScreenCover(isPresented: $showFullScreen) {
+            FullscreenArtworkView(artwork: artwork)
+        }
         
         
     }
@@ -36,6 +49,45 @@ struct ArtworkViewer: View {
 
 #Preview {
     ArtworkViewer(artwork: MockData.artwork_mushroom)
+}
+
+struct FullscreenArtworkView: View {
+    let artwork: Artwork
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        GeometryReader { geo in
+            ZStack(alignment: .center) {
+                Color.black
+                    .opacity(0.8)
+                    .ignoresSafeArea()
+                
+                Grid(horizontalSpacing: 0, verticalSpacing: 0) {
+                    ForEach(0..<artwork.size[1], id: \.self) { row in
+                        GridRow {
+                            ForEach(0..<artwork.size[0], id: \.self) { col in
+                                let index = row * artwork.size[0] + col
+                                Rectangle()
+                                    .fill(Color(hex: artwork.data[index]))
+                                    
+                            }
+                        }
+                    }
+                }
+                .frame(width: geo.size.width, height: geo.size.width)
+            }
+            .overlay(alignment: .topTrailing) {
+                Button {
+                    dismiss()
+                } label: {
+                    Image(systemName: "xmark")
+                        .font(.title)
+                        .foregroundStyle(.white)
+                        .padding()
+                }
+            }
+        }
+    }
 }
 
 /*
